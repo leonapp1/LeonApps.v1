f3ddf0f974792fe6fc06d89380dbfc5df9eef7fd
Edwin Molina <mol13026@gmail.com>
2025-12-20 14:00:50 -0500

    Fix LogoLA.svg import case sensitivity and location

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { cn } from '../../utils/cn';
-import LogoLA from "../../../public/logoLA.svg"
+import React from "react";
+import { cn } from "../../utils/cn";
+import LogoLA from "../../assets/LogoLA.svg";
 
 export const Logo: React.FC<{ className?: string }> = ({ className }) => {
-  return (
-    <div className={cn("", className)}>
-      <img src={LogoLA} alt="Logo LeonApps" className="h-12" />
-    </div>
-  );
+	return (
+		<div className={cn("", className)}>
+			<img src={LogoLA} alt="Logo LeonApps" className="h-12" />
+		</div>
+	);
 };
